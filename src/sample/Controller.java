fc29fb4936ba419ae2689b572d6a9e6dd623a30f
SENSEI <mkarda@gmail.com>
2017-08-20 19:39:52 +0200

    new branch

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
-public class Controller {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Controller extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Label label = new Label("siema");
+        label.setFont(new Font(50));
+
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("first one");
+        primaryStage.show();
+
+
+    }
 }
