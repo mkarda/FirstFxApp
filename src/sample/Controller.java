88a50b936b0206d35e74a6883b630e893af385ac
SENSEI <mkarda@gmail.com>
2017-08-20 20:31:13 +0200

    fields created

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField password;
+
+    public void okAction(ActionEvent actionEvent) {
+        System.out.println("ok clicked!!  " + userName.getText());
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+        System.out.println("CANCEL clicked!!  " + password.getText());
+    }
 }
