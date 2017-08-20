5ae3b249ff65df5588df37c0b7610f1895a76120
SENSEI <mkarda@gmail.com>
2017-08-20 19:50:51 +0200

    fixed controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,23 +1,4 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-
-public class Controller extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        Label label = new Label("siema");
-        label.setFont(new Font(50));
-
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("first one");
-        primaryStage.show();
-
-
-    }
+public class Controller {
 }
